fc68b27a2567d3556af577ca345ad3c18422c5e1
Bolbotunov <100226065+Bolbotunov@users.noreply.github.com>
2023-01-04 16:36:51 +0300

    fix: add ${}

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function toReadable (number) {
-   let units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
   let fromTenToNineteen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen','eighteen', 'nineteen'];
-  let dozens = ['','','twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  let dozens = ['','','twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
     if(number === 0){
     return 'zero'
   } else if(number>0 && number <10){
@@ -9,11 +9,11 @@ module.exports = function toReadable (number) {
   } else if (number > 9 && number < 20){
     return fromTenToNineteen[number % 10]
   } else if (number >19 && number < 100){
-    return dozens[Math.floor(number/10)] + ' ' + units[number%10]
-  } else if (number >100 && number < 1000){
+    return `${dozens[Math.floor(number/10)]} ${units[number%10]}`.trim()
+  } else if (number >99 && number < 1000){
     let firstNumber = units[Math.floor(number/100)]
     let endTheNumber = Math.floor(number%100)
-    return firstNumber + ' hundred ' + toReadable(endTheNumber)
+    return `${firstNumber} hundred ${toReadable(endTheNumber)}`.trim()
   } else{
     return 'one hundred'
   }
