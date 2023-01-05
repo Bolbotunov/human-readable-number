35a498d03aa804f381991dce67e53bf1208c2553
Bolbotunov <100226065+Bolbotunov@users.noreply.github.com>
2023-01-05 12:09:50 +0300

    fix: fix bugs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 module.exports = function toReadable (number) {
+    let stringNumber = String(number);
     let units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-  let fromTenToNineteen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen','eighteen', 'nineteen'];
-  let dozens = ['','','twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let fromTenToNineteen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen','eighteen', 'nineteen'];
+    let dozens = ['','','twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
     if(number === 0){
     return 'zero'
   } else if(number>0 && number <10){
@@ -10,11 +11,13 @@ module.exports = function toReadable (number) {
     return fromTenToNineteen[number % 10]
   } else if (number >19 && number < 100){
     return `${dozens[Math.floor(number/10)]} ${units[number%10]}`.trim()
+  } else if (number >99 && number < 1000 && stringNumber[1] == 0 && stringNumber[2] == 0 ){
+    return units[number/100] + ' hundred ';    
   } else if (number >99 && number < 1000){
     let firstNumber = units[Math.floor(number/100)]
     let endTheNumber = Math.floor(number%100)
     return `${firstNumber} hundred ${toReadable(endTheNumber)}`.trim()
-  } else{
-    return 'one hundred'
+  } else {
+    return 'over 999'
   }
 }
